98fbc7770b60ce525ec09363e5d038f47fe1b9a1
akshaldhal <akshal2272@gmail.com>
2025-03-01 17:50:57 -0500

    changes

--- scripts/main_game.gd
+++ scripts/main_game.gd
@@ -1,14 +1,21 @@
 extends Control
 
+func back():
+	print(Global.last_scene)
+	Global.change_scene(Global.last_scene.back())
+	Global.last_scene.pop_back()
+	print(Global.last_scene)
 
 func  _input(event: InputEvent):
 	if event is InputEventKey:
 		if Input.is_key_pressed(KEY_ESCAPE) and len(Global.last_scene) > 1:
-			print(Global.last_scene)
-			Global.change_scene(Global.last_scene.back())
-			Global.last_scene.pop_back()
-			print(Global.last_scene)
+			back()
+			
 	if event is InputEventMouseMotion:
 		$PointLight2D.position = get_local_mouse_position()
 
-	
+
+
+
+func _on_back_button_pressed() -> void:
+	back()
