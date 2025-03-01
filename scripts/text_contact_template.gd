d6ba746790978e9381d79a1fee32c659b17b190f
akshaldhal <akshal2272@gmail.com>
2025-03-01 16:54:27 -0500

    Update text_contact_template.gd

--- scripts/text_contact_template.gd
+++ scripts/text_contact_template.gd
@@ -2,5 +2,10 @@ extends Button
 
 @export var scene: String 
 
+func _ready() -> void:
+	$AudioStreamPlayer.stream = load("res://audio/clickSound.wav")
+
 func _on_pressed() -> void:
+	$AudioStreamPlayer.play()
 	Global.change_scene(scene)
+	
